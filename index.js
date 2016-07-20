357e65b2298cacec047619e6cf4d3591c9ca9fdb
Athul Raj <athulbraj@gmail.com>
2016-07-20 19:54:02 +0530

    Automatic commit from server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 9007;
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
@@ -20,11 +20,11 @@ io.on('connection', function (socket) {
   var addedUser = false;
 
   // when the client emits 'new message', this listens and executes
-  socket.on('new message', function (data) {
+  socket.on('new location', function (data) {
     // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
+    socket.broadcast.emit('new location', {
       username: socket.username,
-      message: data
+      location: data
     });
   });
 
@@ -46,20 +46,6 @@ io.on('connection', function (socket) {
     });
   });
 
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', function () {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', function () {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
-    });
-  });
-
   // when the user disconnects.. perform this
   socket.on('disconnect', function () {
     if (addedUser) {
@@ -72,5 +58,5 @@ io.on('connection', function (socket) {
       });
     }
   });
-  
+
 });
